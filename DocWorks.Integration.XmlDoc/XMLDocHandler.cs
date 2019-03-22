2d1eaa2f129ee58811739ae4af09a8de4bc73714
Matthew Cooper <matt_cooper1@hotmail.com>
2019-03-22 14:59:50 +0000

    commented out added code

--- DocWorks.Integration.XmlDoc/XMLDocHandler.cs
+++ DocWorks.Integration.XmlDoc/XMLDocHandler.cs
@@ -177,8 +177,8 @@ namespace DocWorks.Integration.XmlDoc
         {
             var xml = typeSymbol.GetDocumentationCommentXml();
             xml = extraMemberRegEx.Replace(xml, "");
-            xml = string.Join("\r\n", xml.Split(new[] {"\r\n", "\n"}, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Substring(4)));
-            xml = XmlUtility.LegalString(xml);
+            //xml = string.Join("\r\n", xml.Split(new[] {"\r\n", "\n"}, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Substring(4)));
+            //xml = XmlUtility.LegalString(xml);
             //escape end of CDATA tags
             xml = xml.Replace("]]>", "]]]]><![CDATA[>");
             return $@"<![CDATA[{xml}]]>";
@@ -358,7 +358,7 @@ namespace DocWorks.Integration.XmlDoc
 {accessibilityXml}
 {typeXml}";
                     }
-                case SymbolKind.Method:
+                case SymbolKind.Method: // here is where you fix refs
                     {
                         var method = (IMethodSymbol)member;
                         string returnXml = "";
